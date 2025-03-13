9fba37a078e200d2e4452d1ca63d4e87f62e64c8
Qwexaro <Sergikstar25.gold@mail.ru>
2025-03-13 12:21:14 +0300

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,23 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
 
-        mapStudents.put("qwx.s.i@mail.ru",
-                new Student("Sergey", 24, "qwx.s.i@mail.ru", 8987667876l)
-        );
+        new Student("Sergey", 24, "qwx.s.i@mail.ru", 8987667876l);
 
-        mapStudents.put("qwx.s.i@mail.ru",
-                new Student("Boris", 23, "qwx.s.i@mail.ru", 8987667876l)
-        );
 
-        mapStudents.put("qwx.s.i@mail.ru",
-                new Student("Kuznechik", 21, "qwx.s.i@mail.ru", 8987667876l)
-        );
+        new Student("Boris", 23, "qwx.s.i@mail.ru", 8987667876l);
 
-        for (Map.Entry<String, Student> currentEmailStudent : mapStudents.entrySet()) {
-            String currentEmail = currentEmailStudent.getKey(); // first parameters
-            Student currentStudent = currentEmailStudent.getValue(); // second parameters
-            System.out.println(currentEmail + " - " + currentStudent);
-        }
+
+        new Student("Kuznechik", 21, "qwx.s.i@mail.ru", 8987667876l);
+
+        Student.removeSelectedStudent(8987667876l);
+        Student.getListAllStudents();
 
     }
 }
