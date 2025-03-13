1448b065093a634ef814c30b3235da168456514b
Qwexaro <Sergikstar25.gold@mail.ru>
2025-03-13 12:07:14 +0300

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,17 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         HashMap<String, Student> mapStudents = new HashMap<>();
+        mapStudents.put("qwx.s.i@mail.ru",
+                new Student("Sergey", 24, "qwx.s.i@mail.ru", 8987667876l)
+        );
+
+        mapStudents.put("qwx.s.i@mail.ru",
+                new Student("Boris", 23, "qwx.s.i@mail.ru", 8987667876l)
+        );
+
+        mapStudents.put("qwx.s.i@mail.ru",
+                new Student("Kuznechik", 21, "qwx.s.i@mail.ru", 8987667876l)
+        );
+
     }
 }
